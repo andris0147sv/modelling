a1a3b57d36487292669857317875b3090c7306a0
Jenyay <jenyay.ilin@gmail.com>
2018-12-04 21:00:34 +0300

    Добавлены примеры FDTD для MATLAB в кодировке cp1251 (matlab/windows)

---
+++ matlab/windows/fdtd_dispersion.m
@@ -0,0 +1,74 @@
+%% Одномерный FDTD. Версия 1.3
+% Гауссов импульс распространяется в одну сторону (TFSF boundary)
+clear
+
+% Волновое сопротивление свободного пространства
+W0 = 120 * pi;
+
+% Время расчета в отсчетах
+maxTime = 1000;
+
+% Размер области моделирования в отсчетах
+maxSize = 500;
+
+% Положение датчика, регистрирующего поля
+probePos = 60;
+
+% Положение источника возбуждения
+sourcePos = 50;
+
+Sc = 0.5;
+
+Ez = zeros (1, maxSize);
+Hy = zeros (1, maxSize);
+
+% Поле, зарегистрированное в датчике в зависимости от времени
+probeTimeEz = zeros (1, maxTime);
+
+figure
+
+for t = 1: maxTime
+    % Расчет компоненты поля H
+    Hy(maxSize) = Hy(maxSize - 1);
+    for m = 1: maxSize - 1
+        % До этой строки Hy(n) хранит значение компоненты Hy
+        % за предыдущий момент времени
+        Hy(m) = Hy(m) + (Ez(m + 1) - Ez(m)) * Sc / W0;
+    end
+    
+    Hy(sourcePos - 1) = Hy(sourcePos - 1) -...
+        exp (-(t - 30.0) ^ 2 / 100.0) * Sc / W0;
+    
+    % Расчет компоненты поля E
+    Ez(1) = Ez(2);
+    for m = 2: maxSize
+        % До этой строки Ez(n) хранит значение компоненты EzS
+        % за предыдущий момент времени
+        Ez(m) = Ez(m) + (Hy(m) - Hy(m - 1)) * Sc * W0;
+    end
+
+    % Источник возбуждения
+    Ez(sourcePos) = Ez(sourcePos) +...
+        exp (-(t + 0.5 - (-0.5) - 30.0) ^ 2 / 100.0) * Sc;
+    
+    % Регистрация поля в точке
+    probeTimeEz(t) = Ez(probePos);
+    
+    plot (Ez);
+    xlim ([1, maxSize]);
+    ylim ([-1.1, 1.1]);
+    xlabel ('x, отсчет')
+    ylabel ('Ez, В/м')
+    grid on
+    hold on
+    plot (probePos, 0, 'xk');
+    plot (sourcePos, 0, '*r');
+    hold off
+    pause (0.01)
+end
+
+figure
+plot (probeTimeEz)
+xlabel ('t, отсчет')
+ylabel ('Ez, В/м')
+grid on
