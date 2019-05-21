a30f317042acbef9cbc32ff38953832487057e29
Eugeniy Ilin <jenyay.ilin@gmail.com>
2019-05-21 14:28:21 +0300

    Первые варианты FDTD 1D написан на Python

---
+++ python/fdtd_first_version_01.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import pylab
+import numpy
+
+if __name__ == '__main__':
+    # Волновое сопротивление свободного пространства
+    W0 = 120.0 * numpy.pi
+
+    # Число Куранта
+    Sc = 1.0
+
+    # Время расчета в отсчетах
+    maxTime = 1000
+
+    # Размер области моделирования в отсчетах
+    maxSize = 200
+
+    # Положение датчика, регистрирующего поля
+    probePos = 50
+
+    Ez = numpy.zeros(maxSize)
+    Hy = numpy.zeros(maxSize)
+
+    # Поле, зарегистрированное в датчике в зависимости от времени
+    probeTimeEz = numpy.zeros(maxTime)
+
+    for t in range(maxTime):
+        # Расчет компоненты поля H
+        for x in range(0, maxSize - 1):
+            Hy[x] = Hy[x] + (Ez[x + 1] - Ez[x]) * Sc / W0
+
+        # Расчет компоненты поля E
+        for x in range(1, maxSize):
+            Ez[x] = Ez[x] + (Hy[x] - Hy[x - 1]) * Sc * W0
+
+        # Источник возбуждения
+        Ez[0] = numpy.exp(-(t - 30.0) ** 2 / 100.0)
+
+        # Регистрация поля в точке
+        probeTimeEz[t] = Ez[probePos]
+
+    # Отображение сигнала, сохраненного в пробнике
+    tlist = numpy.arange(maxTime)
+    fig, ax = pylab.subplots()
+    ax.set_xlim(0, maxTime)
+    ax.set_ylim(-1.1, 1.1)
+    ax.set_xlabel('t, отсчет')
+    ax.set_ylabel('Ez, В/м')
+    ax.plot(tlist, probeTimeEz)
+    ax.grid()
+    pylab.show()
--- python/fdtd_first_version_02.py
+++ python/fdtd_first_version_02.py
@@ -25,23 +25,40 @@ if __name__ == '__main__':
     # Поле, зарегистрированное в датчике в зависимости от времени
     probeTimeEz = numpy.zeros(maxTime)
 
+    # Подготовка к отображению поля в пространстве
     xlist = numpy.arange(maxSize)
+
+    # Включить интерактивный режим для анимации
     pylab.ion()
+
+    # Создание окна для графика
     fig, ax = pylab.subplots()
+
+    # Установка отображаемых интервалов по осям
     ax.set_xlim(0, maxSize)
     ax.set_ylim(-1.1, 1.1)
-    ax.set_xlabel('t, отсчет')
+
+    # Установка меток по осям
+    ax.set_xlabel('x, отсчет')
     ax.set_ylabel('Ez, В/м')
+
+    # Включить сетку на графике
+    ax.grid()
+
+    # Отобразить поле в начальный момент времени
     line, = ax.plot(xlist, Ez)
+
+    # Отобразить положение пробника
+    ax.plot(probePos, 0, 'xr')
 
     for t in range(maxTime):
         # Расчет компоненты поля H
-        Ez_shift = Ez[1:]
-        Hy[:-1] = Hy[:-1] + (Ez_shift - Ez[:-1]) * Sc / W0
+        for x in range(0, maxSize - 1):
+            Hy[x] = Hy[x] + (Ez[x + 1] - Ez[x]) * Sc / W0
 
         # Расчет компоненты поля E
-        Hy_shift = Hy[:-1]
-        Ez[1:] = Ez[1:] + (Hy[1:] - Hy_shift) * Sc * W0
+        for x in range(1, maxSize):
+            Ez[x] = Ez[x] + (Hy[x] - Hy[x - 1]) * Sc * W0
 
         # Источник возбуждения
         Ez[0] = numpy.exp(-(t - 30.0) ** 2 / 100.0)
@@ -50,8 +67,21 @@ if __name__ == '__main__':
         probeTimeEz[t] = Ez[probePos]
 
         if t % 2 == 0:
+            # Обновить данные на графике
             line.set_ydata(Ez)
             fig.canvas.draw()
             fig.canvas.flush_events()
 
-    pylab.close()
+    # Отключить интерактивный режим по завершению анимации
+    pylab.ioff()
+
+    # Отображение сигнала, сохраненного в пробнике
+    tlist = numpy.arange(maxTime)
+    fig, ax = pylab.subplots()
+    ax.set_xlim(0, maxTime)
+    ax.set_ylim(-1.1, 1.1)
+    ax.set_xlabel('t, отсчет')
+    ax.set_ylabel('Ez, В/м')
+    ax.plot(tlist, probeTimeEz)
+    ax.grid()
+    pylab.show()
