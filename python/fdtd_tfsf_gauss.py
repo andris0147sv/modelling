a034a0038a953a543120a6525fdb72b560c9d3e9
Eugene Ilin <jenyay.ilin@gmail.com>
2019-06-14 20:49:32 +0300

    Python. Добавлен пример метода Total Field / Scattered Field для гауссова сигнала

---
+++ python/fdtd_tfsf_gauss.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+'''
+Прстейшие граничные условия
+'''
+
+import numpy
+
+import tools
+
+
+if __name__ == '__main__':
+    # Волновое сопротивление свободного пространства
+    W0 = 120.0 * numpy.pi
+
+    # Число Куранта
+    Sc = 1.0
+
+    # Время расчета в отсчетах
+    maxTime = 400
+
+    # Размер области моделирования в отсчетах
+    maxSize = 200
+
+    # Положение источника в отсчетах
+    sourcePos = 50
+
+    # Датчики для регистрации поля
+    probesPos = [75]
+    probes = [tools.Probe(pos, maxTime) for pos in probesPos]
+
+    Ez = numpy.zeros(maxSize)
+    Hy = numpy.zeros(maxSize)
+
+    # Параметры отображения поля E
+    display_field = Ez
+    display_ylabel = 'Ez, В/м'
+    display_ymin = -1.1
+    display_ymax = 1.1
+
+    # Создание экземпляра класса для отображения
+    # распределения поля в пространстве
+    display = tools.AnimateFieldDisplay(maxSize,
+                                        display_ymin, display_ymax,
+                                        display_ylabel)
+
+    display.activate()
+    display.drawProbes(probesPos)
+    display.drawSources([sourcePos])
+
+    for t in range(maxTime):
+        # Граничные условия для поля H
+        Hy[-1] = Hy[-2]
+
+        # Расчет компоненты поля H
+        Ez_shift = Ez[1:]
+        Hy[:-1] = Hy[:-1] + (Ez_shift - Ez[:-1]) * Sc / W0
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        # Hy[sourcePos - 1] -= numpy.exp(-(t - 30.0 - sourcePos) ** 2 / 100.0) / W0;
+
+        Hy[sourcePos - 1] -= numpy.exp(-(t - 30.0) ** 2 / 100.0) / W0;
+
+        # Граничные условия для поля E
+        Ez[0] = Ez[1]
+
+        # Расчет компоненты поля E
+        Hy_shift = Hy[:-1]
+        Ez[1:] = Ez[1:] + (Hy[1:] - Hy_shift) * Sc * W0
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        # Ez[sourcePos] += numpy.exp(-((t + 0.5) - (sourcePos - 0.5) - 30.0) ** 2 / 100.0);
+
+        Ez[sourcePos] += numpy.exp(-((t + 0.5) - (-0.5) - 30.0) ** 2 / 100.0);
+
+        # Регистрация поля в датчиках
+        for probe in probes:
+            probe.addData(Ez, Hy)
+
+        if t % 2 == 0:
+            display.updateData(display_field, t)
+
+    display.stop()
+
+    # Отображение сигнала, сохраненного в пробнике
+    tools.showProbeSignals(probes, -1.1, 1.1)
