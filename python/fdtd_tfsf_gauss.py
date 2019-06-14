77f7345d92752d238b99f5ab7566f1ceaeaf1db9
Eugene Ilin <jenyay.ilin@gmail.com>
2019-06-14 21:11:48 +0300

    Подчистка кода

--- python/fdtd_tfsf_gauss.py
+++ python/fdtd_tfsf_gauss.py
@@ -57,9 +57,9 @@ if __name__ == '__main__':
 
         # Источник возбуждения с использованием метода
         # Total Field / Scattered Field
-        # Hy[sourcePos - 1] -= numpy.exp(-(t - 30.0 - sourcePos) ** 2 / 100.0) / W0;
+        # Hy[sourcePos - 1] -= numpy.exp(-(t - 30.0 - sourcePos) ** 2 / 100.0) / W0
 
-        Hy[sourcePos - 1] -= numpy.exp(-(t - 30.0) ** 2 / 100.0) / W0;
+        Hy[sourcePos - 1] -= numpy.exp(-(t - 30.0) ** 2 / 100.0) / W0
 
         # Граничные условия для поля E
         Ez[0] = Ez[1]
@@ -70,9 +70,9 @@ if __name__ == '__main__':
 
         # Источник возбуждения с использованием метода
         # Total Field / Scattered Field
-        # Ez[sourcePos] += numpy.exp(-((t + 0.5) - (sourcePos - 0.5) - 30.0) ** 2 / 100.0);
+        # Ez[sourcePos] += numpy.exp(-((t + 0.5) - (sourcePos - 0.5) - 30.0) ** 2 / 100.0)
 
-        Ez[sourcePos] += numpy.exp(-((t + 0.5) - (-0.5) - 30.0) ** 2 / 100.0);
+        Ez[sourcePos] += numpy.exp(-((t + 0.5) - (-0.5) - 30.0) ** 2 / 100.0)
 
         # Регистрация поля в датчиках
         for probe in probes:
