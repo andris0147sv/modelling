3d2afab9b8c004ecf71bce390db2a3bdf3c7e91c
Eugene Ilin <jenyay.ilin@gmail.com>
2019-06-23 21:18:34 +0300

    Python. Добавлен пример с модулированным гауссовым импульсом. Источник находится в диэлектрике

---
+++ python/fdtd_tfsf_medium_gauss_mod.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+'''
+ЭМ волна в виде модулированного гассова импульса распространяется
+в одну сторону (TFSF boundary).
+Источник находится в диэлектрике.
+'''
+
+import numpy
+
+import tools
+
+
+if __name__ == '__main__':
+    # Волновое сопротивление свободного пространства
+    W0 = 120.0 * numpy.pi
+
+    # Число Куранта
+    Sc = 1.0
+
+    # Время расчета в отсчетах
+    maxTime = 500
+
+    # Размер области моделирования в отсчетах
+    maxSize = 200
+
+    # Положение источника в отсчетах
+    sourcePos = 50
+
+    # Параметры сигнала
+    Nl = 40
+    Nwg = 50
+    Ndg = 150
+
+    # Датчики для регистрации поля
+    probesPos = [60]
+    probes = [tools.Probe(pos, maxTime) for pos in probesPos]
+
+    # Параметры среды
+    # Диэлектрическая проницаемость
+    eps = numpy.ones(maxSize)
+    eps[:] = 9.0
+
+    # Магнитная проницаемость
+    mu = numpy.ones(maxSize - 1)
+
+    Ez = numpy.zeros(maxSize)
+    Hy = numpy.zeros(maxSize - 1)
+
+    # Параметры отображения поля E
+    display_field = Ez
+    display_ylabel = 'Ez, В/м'
+    display_ymin = -1.1
+    display_ymax = 1.1
+
+    # Создание экземпляра класса для отображения
+    # распределения поля в пространстве
+    display = tools.AnimateFieldDisplay(maxSize,
+                                        display_ymin, display_ymax,
+                                        display_ylabel)
+
+    display.activate()
+    display.drawProbes(probesPos)
+    display.drawSources([sourcePos])
+
+    for t in range(maxTime):
+        # Расчет компоненты поля H
+        Hy = Hy + (Ez[1:] - Ez[:-1]) * Sc / (W0 * mu)
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        Hy[sourcePos - 1] -= (Sc / (W0 * mu[sourcePos - 1]) *
+                              numpy.sin(2 * numpy.pi * t * Sc / Nl) *
+                              numpy.exp(-(t - Ndg) ** 2 / (Nwg ** 2)))
+
+        # Граничные условия для поля E
+        Ez[0] = Ez[1]
+        Ez[-1] = Ez[-2]
+
+        # Расчет компоненты поля E
+        Hy_shift = Hy[:-1]
+        Ez[1:-1] = Ez[1:-1] + (Hy[1:] - Hy_shift) * Sc * W0 / eps[1:-1]
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        Ez[sourcePos] += (Sc / (numpy.sqrt(eps[sourcePos] * mu[sourcePos])) *
+                          numpy.sin(2 * numpy.pi / Nl * ((t + 0.5) * Sc - (-0.5 * numpy.sqrt(eps[sourcePos] * mu[sourcePos])))) *
+                          numpy.exp(-(t + 0.5 - (-0.5 * numpy.sqrt(eps[sourcePos] * mu[sourcePos]) / Sc) - Ndg) ** 2 / (Nwg ** 2)))
+
+        # Регистрация поля в датчиках
+        for probe in probes:
+            probe.addData(Ez, Hy)
+
+        if t % 2 == 0:
+            display.updateData(display_field, t)
+
+    display.stop()
+
+    # Отображение сигнала, сохраненного в пробнике
+    tools.showProbeSignals(probes, -1.1, 1.1)
