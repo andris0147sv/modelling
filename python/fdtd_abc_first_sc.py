608ed978b3cfa603e673a64ffe33bdb1487323fc
Eugene Ilin <jenyay.ilin@gmail.com>
2020-04-26 13:08:13 +0300

    Добавлены примеры с неравномерной сеткой

---
+++ python/fdtd_abc_first_sc.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+'''
+Моделирование распространения ЭМ волны, падающей на границу
+вакуум - идеальный диэлектрик.
+Используются граничные условия ABC первой степени.
+
+Неравномерная сетка (Sc зависит от индекса)
+'''
+
+import numpy
+
+import tools
+
+
+if __name__ == '__main__':
+    # Волновое сопротивление свободного пространства
+    W0 = 120.0 * numpy.pi
+
+    # Время расчета в отсчетах
+    maxTime = 750
+
+    # Размер области моделирования в отсчетах
+    maxSize = 200
+
+    # Положение источника в отсчетах
+    sourcePos = 50
+
+    # Датчики для регистрации поля
+    probesPos = [160]
+    probes = [tools.Probe(pos, maxTime) for pos in probesPos]
+
+    # Положение начала диэлектрика
+    layer_x = 100
+
+    # Параметры среды
+    # Диэлектрическая проницаемость
+    eps = numpy.ones(maxSize)
+    eps[layer_x:] = 9.0
+
+    # Магнитная проницаемость
+    mu = numpy.ones(maxSize)
+    Sc = numpy.sqrt(eps * mu)
+
+    Ez = numpy.zeros(maxSize)
+    Hy = numpy.zeros(maxSize - 1)
+
+    # Ez[1] в предыдущий момент времени
+    oldEzLeft = Ez[1]
+
+    # Ez[-2] в предыдущий момент времени
+    oldEzRight = Ez[-2]
+
+    # Расчет коэффициентов для граничных условий
+    tempLeft = Sc[0] / numpy.sqrt(mu[0] * eps[0])
+    koeffABCLeft = (tempLeft - 1) / (tempLeft + 1)
+
+    tempRight = Sc[-1] / numpy.sqrt(mu[-1] * eps[-1])
+    koeffABCRight = (tempRight - 1) / (tempRight + 1)
+
+    # Параметры отображения поля E
+    display_field = Ez
+    display_ylabel = 'Ez, В/м'
+    display_ymin = -1.1
+    display_ymax = 1.1
+
+    # Создание экземпляра класса для отображения
+    # распределения поля в пространстве
+    display = tools.AnimateFieldDisplay(maxSize,
+                                        display_ymin, display_ymax,
+                                        display_ylabel)
+
+    display.activate()
+    display.drawProbes(probesPos)
+    display.drawSources([sourcePos])
+    display.drawBoundary(layer_x)
+
+    for t in range(maxTime):
+        # Расчет компоненты поля H
+        Hy = Hy + (Ez[1:] - Ez[:-1]) * Sc[:-1] / (W0 * mu[:-1])
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        Hy[sourcePos - 1] -= (Sc[sourcePos - 1] / (W0 * mu[sourcePos - 1]) *
+                              numpy.exp(-(t - 30.0) ** 2 / 100.0))
+
+        # Граничные условия ABC в упрощенном виде
+        # Ez[0] = Ez[1]
+        # Ez[-1] = Ez[-2]
+
+        # Расчет компоненты поля E
+        Hy_shift = Hy[: -1]
+        Ez[1:-1] = Ez[1: -1] + (Hy[1:] - Hy_shift) * Sc[1: -1] * W0 / eps[1: -1]
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        Ez[sourcePos] += (Sc[sourcePos] / (numpy.sqrt(eps[sourcePos] * mu[sourcePos])) *
+                          numpy.exp(-((t + 0.5) - (-0.5 * numpy.sqrt(eps[sourcePos] * mu[sourcePos]) / Sc[sourcePos]) - 30.0) ** 2 / 100.0))
+
+        # Граничные условия ABC первой степени
+        Ez[0] = oldEzLeft + koeffABCLeft * (Ez[1] - Ez[0])
+        oldEzLeft = Ez[1]
+
+        Ez[-1] = oldEzRight + koeffABCRight * (Ez[-2] - Ez[-1])
+        oldEzRight = Ez[-2]
+
+        # Регистрация поля в датчиках
+        for probe in probes:
+            probe.addData(Ez, Hy)
+
+        if t % 2 == 0:
+            display.updateData(display_field, t)
+
+    display.stop()
+
+    # Отображение сигнала, сохраненного в датчиках
+    tools.showProbeSignals(probes, -1.1, 1.1)
---
+++ python/fdtd_heterogen_sc.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+'''
+Моделирование распространения ЭМ волны, падающей на границу
+вакуум - идеальный диэлектрик.
+
+Неравномерная сетка (Sc зависит от индекса)
+'''
+
+import numpy
+
+import tools
+
+
+if __name__ == '__main__':
+    # Волновое сопротивление свободного пространства
+    W0 = 120.0 * numpy.pi
+
+    # Время расчета в отсчетах
+    maxTime = 600
+
+    # Размер области моделирования в отсчетах
+    maxSize = 600
+
+    # Положение источника в отсчетах
+    sourcePos = 50
+
+    # Датчики для регистрации поля
+    probesPos = [75]
+    probes = [tools.Probe(pos, maxTime) for pos in probesPos]
+
+    # Положение начала диэлектрика
+    layer_x = 100
+
+    # Параметры среды
+    # Диэлектрическая проницаемость
+    eps = numpy.ones(maxSize)
+    eps[layer_x:] = 9.0
+
+    # Магнитная проницаемость
+    mu = numpy.ones(maxSize)
+
+    Sc = numpy.sqrt(eps * mu)
+
+    Ez = numpy.zeros(maxSize)
+    Hy = numpy.zeros(maxSize)
+
+    # Параметры отображения поля E
+    display_field = Ez
+    display_ylabel = 'Ez, В/м'
+    display_ymin = -1.1
+    display_ymax = 1.1
+
+    # Создание экземпляра класса для отображения
+    # распределения поля в пространстве
+    display = tools.AnimateFieldDisplay(maxSize,
+                                        display_ymin, display_ymax,
+                                        display_ylabel)
+
+    display.activate()
+    display.drawProbes(probesPos)
+    display.drawSources([sourcePos])
+    display.drawBoundary(layer_x)
+
+    for t in range(maxTime):
+        # Граничные условия для поля H
+        Hy[-1] = Hy[-2]
+
+        # Расчет компоненты поля H
+        Ez_shift = Ez[1:]
+        Hy[:-1] = Hy[:-1] + (Ez_shift - Ez[:-1]) * Sc[:-1] / (W0 * mu[:-1])
+
+        # Граничные условия для поля E
+        Ez[0] = Ez[1]
+
+        # Расчет компоненты поля E
+        Hy_shift = Hy[:-1]
+        Ez[1:] = Ez[1:] + (Hy[1:] - Hy_shift) * Sc[1:] * W0 / eps[1:]
+
+        # Источник возбуждения
+        Ez[sourcePos] += numpy.exp(-(t - 0.5 - 30.0) ** 2 / 100.0)
+
+        # Регистрация поля в датчиках
+        for probe in probes:
+            probe.addData(Ez, Hy)
+
+        if t % 2 == 0:
+            display.updateData(display_field, t)
+
+    display.stop()
+
+    # Отображение сигнала, сохраненного в датчиках
+    tools.showProbeSignals(probes, -1.1, 1.1)
